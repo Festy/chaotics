b18785e3d18c56cc3d0f33108987f2fea28962f5
Utsav Patel <utsav.patel@citrix.com>
2017-02-26 23:05:08 -0800

    replace contraints with rodes

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,15 +1,22 @@
 var Engine = Matter.Engine,
     Render = Matter.Render,
     Runner = Matter.Runner,
-    Composites = Matter.Composites,
     MouseConstraint = Matter.MouseConstraint,
     Mouse = Matter.Mouse,
     World = Matter.World,
     Constraint = Matter.Constraint,
     Bodies = Matter.Bodies;
 
-var engine = Engine.create(),
-    world = engine.world;
+var engine = Engine.create();
+    engine.world = World.create({
+    gravity: {
+        x: 0,
+        y: 1,
+        scale: 0.005
+    }
+});
+var world = engine.world;
+
 
 var render = Render.create({
     element: document.body,
@@ -17,7 +24,6 @@ var render = Render.create({
     options: {
         width: Math.min(document.documentElement.clientWidth),
         height: Math.min(document.documentElement.clientHeight),
-        showAngleIndicator: false
     }
 });
 
@@ -27,13 +33,33 @@ Render.run(render);
 var runner = Runner.create();
 Runner.run(runner, engine);
 
-var ball = Bodies.circle(Math.min(document.documentElement.clientWidth)/2, Math.min(document.documentElement.clientHeight)/2 + 100, 50, { density: 0.04, frictionAir: 0.005});
+//
 
-World.add(world, ball);
+var topLocation = {
+    x : Math.min(document.documentElement.clientWidth)/2,
+    y : Math.min(document.documentElement.clientHeight)/2
+}
+
+var ball1 = Bodies.rectangle(topLocation.x, topLocation.y + 50, 10, 100, { density: 0.1, friction:0, frictionAir: 0, collisionFilter: {group: -1}});
+var ball2 = Bodies.rectangle(topLocation.x, topLocation.y + 140, 10, 100, { density: 0.1, friction:0, frictionAir: 0, collisionFilter: {group: -1}});
+
+
+World.add(world, ball1);
+World.add(world, ball2);
 
 World.add(world, Constraint.create({
-    pointA: { x: Math.min(document.documentElement.clientWidth)/2, y: Math.min(document.documentElement.clientHeight)/2 },
-    bodyB: ball
+    pointA: { x: topLocation.x , y: topLocation.y},
+    pointB: { x: 0, y: -50},
+    bodyB: ball1,
+    stiffness: 1
+}));
+
+World.add(world, Constraint.create({
+    bodyA: ball1,
+    pointA: { x: 0, y: 45},
+    bodyB: ball2,
+    pointB: { x: 0, y: -45},
+    stiffness: 1
 }));
 
 // add mouse control
@@ -41,7 +67,7 @@ var mouse = Mouse.create(render.canvas),
     mouseConstraint = MouseConstraint.create(engine, {
         mouse: mouse,
         constraint: {
-            stiffness: 0.2,
+            stiffness: 1,
             render: {
                 visible: false
             }
